daa788c31c131f0e7ce42be80bea8a13191adb97
deet5 <idenismiller@gmail.com>
2023-03-28 10:44:20 -0700

    Fixed CMakeLists.txt

--- test/hotChoclateTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "../lib/HotChocolate.h"
-#include <string>
-
-#include "gtest/gtest.h"
-
-TEST(HCTEST, NameTest1){
-   HotChocolate hotChocolate1;
-   EXPECT_EQ(hotChocolate1.getName(), "Hot Chocolate");
-}
